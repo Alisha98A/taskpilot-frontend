4e5bd799aaf8de22bb1f8d3f6da29f00e2e556f2
Alisha <alishaanderssonn@icloud.com>
2025-05-18 20:02:48 +0200

    fix: highlight active NavLink in NavBar using activeClassName and exact props

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,33 +1,34 @@
-import React from 'react'
-import { Navbar, Container, Nav} from 'react-bootstrap';
-import styles from '../styles/NavBar.module.css'
+import React from 'react';
+import { Navbar, Container, Nav } from 'react-bootstrap';
+import styles from '../styles/NavBar.module.css';
 import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
-        <NavLink to="/">
-        <Navbar.Brand>
-          <img src="/favicon_io/logo.png" alt="logo" height="45" />
-        </Navbar.Brand></NavLink>
+        <NavLink to="/" exact activeClassName={styles.Active} className={styles.navLink}>
+          <Navbar.Brand>
+            <img src="/favicon_io/logo.png" alt="logo" height="45" />
+          </Navbar.Brand>
+        </NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className={`${styles.navLinks} ml-auto text-left`}>
-            <NavLink to="/" className={styles.navLink}>
+            <NavLink to="/" exact activeClassName={styles.Active} className={styles.navLink}>
               <i className="fas fa-home"></i>Home
             </NavLink>
-            <NavLink to="/signin" className={styles.navLink}>
+            <NavLink to="/signin" activeClassName={styles.Active} className={styles.navLink}>
               <i className="fas fa-sign-in-alt"></i>Sign in
             </NavLink>
-            <NavLink to="/" className={styles.navLink}>
+            <NavLink to="/signup" activeClassName={styles.Active} className={styles.navLink}>
               <i className="fas fa-user-plus"></i>Sign up
             </NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
