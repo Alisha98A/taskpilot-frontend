78006f75b6402be87338f3dbafd0df87cd74bd40
Alisha <alishaanderssonn@icloud.com>
2025-05-18 19:07:21 +0200

    Style navbar with color & effects

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,23 +1,24 @@
 import React from 'react'
 import { Navbar, Container, Nav} from 'react-bootstrap';
+import styles from '../styles/NavBar.module.css'
 
 const NavBar = () => {
   return (
-    <Navbar expand="md" fixed="top">
+    <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
         <Navbar.Brand>
-        <img src="/favicon_io/logo.png" alt="logo" height="45" />
+          <img src="/favicon_io/logo.png" alt="logo" height="45" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ml-auto text-left">
-            <Nav.Link>
+          <Nav className={`${styles.navLinks} ml-auto text-left`}>
+            <Nav.Link className={styles.navLink}>
               <i className="fas fa-home"></i>Home
             </Nav.Link>
-            <Nav.Link>
+            <Nav.Link className={styles.navLink}>
               <i className="fas fa-sign-in-alt"></i>Sign in
             </Nav.Link>
-            <Nav.Link>
+            <Nav.Link className={styles.navLink}>
               <i className="fas fa-user-plus"></i>Sign up
             </Nav.Link>
           </Nav>
