c1886f37ab93cff55e1d7c5316f5c9c6f34bc53b
Alisha <alishaanderssonn@icloud.com>
2025-05-19 21:46:14 +0200

    Import axios and SignUpForm to app.js

---
+++ src/api/axiosDefaults.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+axios.defaults.baseURL = 'https://taskpilot-backend-6ee557f05c5b.herokuapp.com/';
+axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
+axios.defaults.withCredentials = true;
