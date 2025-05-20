951b962fc5effd02abf339d33bded8b7e7a44914
Alisha <alishaanderssonn@icloud.com>
2025-05-20 17:42:32 +0200

    Add left column layout

--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -10,7 +10,9 @@ import appStyles from "../../App.module.css";
 // Reusable form input
 const SignUpForm = () => {
   return (
-    <Row className={styles.Row}></Row>
+    <Row className={styles.Row}>
+      <Col className="my-auto py-4 px-3 px-md-5" md={6}></Col>
+    </Row>
   );
 };
 
