390662a101dc5e4cf2c0e77feb74ed949b791277
Alisha <alishaanderssonn@icloud.com>
2025-05-20 17:34:33 +0200

    Import CSS modules

--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -1,3 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Form, Button, Image, Col, Row, Container } from "react-bootstrap";
+
+// Styles
+import styles from "../../styles/SignInUpForm.module.css";
+import btnStyles from "../../styles/Button.module.css";
+import appStyles from "../../App.module.css";
