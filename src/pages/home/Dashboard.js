7d3b458c8d8c265085037bbcaa30ff72579ad708
Alisha <alishaanderssonn@icloud.com>
2025-05-26 17:51:58 +0200

    add pending tasks and saved notes cards

--- src/pages/home/Dashboard.js
+++ src/pages/home/Dashboard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FaClipboardList, FaRegStickyNote } from 'react-icons/fa';
 import styles from '../../styles/Dashboard.module.css';
 
 function Dashboard() {
@@ -12,7 +13,27 @@ function Dashboard() {
         </p>
       </header>
 
-      <section className={styles.statsSection}></section>
+      <section className={styles.statsSection}>
+        <Link to="/tasks" className={styles.statCard}>
+          <div className={styles.icon}>
+            <FaClipboardList size={32} />
+          </div>
+          <div>
+            <h2>Pending Tasks</h2>
+            <p>8 Tasks</p>
+          </div>
+        </Link>
+
+        <Link to="/notes" className={styles.statCard}>
+          <div className={styles.icon}>
+            <FaRegStickyNote size={32} />
+          </div>
+          <div>
+            <h2>Saved Notes</h2>
+            <p>12 Notes</p>
+          </div>
+        </Link>
+      </section>
     </main>
   );
 }
