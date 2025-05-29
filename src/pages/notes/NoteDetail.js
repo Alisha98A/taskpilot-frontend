4a18d9b852c30357e6065ccd39975f663ab5447a
Alisha <alishaanderssonn@icloud.com>
2025-05-29 22:57:59 +0200

    Add note fetching with useParams

--- src/pages/notes/NoteDetail.js
+++ src/pages/notes/NoteDetail.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { axiosReq } from "../../api/axiosDefaults";
 
 function NoteDetail() {
-  return <div>Note Detail</div>;
+  const { id } = useParams();
+  const [note, setNote] = useState(null);
+
+  useEffect(() => {
+    const fetchNote = async () => {
+      try {
+        const { data } = await axiosReq.get(`/api/notes/${id}/`);
+        setNote(data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    fetchNote();
+  }, [id]);
+
+  return <div>{note ? note.body : "Loading..."}</div>;
 }
 
 export default NoteDetail;
