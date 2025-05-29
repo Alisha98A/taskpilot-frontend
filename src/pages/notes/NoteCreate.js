4370b90fc217abfe8e521012e29301cf1573de8f
Alisha <alishaanderssonn@icloud.com>
2025-05-29 23:03:13 +0200

    Add NoteCreate component structure

---
+++ src/pages/notes/NoteCreate.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+function NoteCreate() {
+  return (
+    <Container className="my-4">
+      <h2>Add Note</h2>
+    </Container>
+  );
+}
+
+export default NoteCreate;
