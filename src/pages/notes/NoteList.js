c9363a0f9880a1a7426322c6d7d954b58d894382
Alisha <alishaanderssonn@icloud.com>
2025-05-29 22:32:37 +0200

    Add loading and error states

--- src/pages/notes/NoteList.js
+++ src/pages/notes/NoteList.js
@@ -1,10 +1,23 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Container, Spinner, Alert } from "react-bootstrap";
+import useNotesList from "../../hooks/useNotesList";
 
 function NoteList() {
+  const { notes, loading, error } = useNotesList();
+
+  if (loading) {
+    return (
+      <Container className="text-center mt-5">
+        <Spinner animation="border" />
+        <p>Loading notes...</p>
+      </Container>
+    );
+  }
+
   return (
     <Container className="my-4">
       <h2>Notes</h2>
+      {error && <Alert variant="danger">{error}</Alert>}
     </Container>
   );
 }
