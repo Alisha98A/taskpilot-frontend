b688f43822014f9ac490c43ab1cb795b1e284301
Alisha <alishaanderssonn@icloud.com>
2025-05-29 23:17:42 +0200

    Use form hook for note editing

--- src/pages/notes/NoteEdit.js
+++ src/pages/notes/NoteEdit.js
@@ -1,10 +1,20 @@
 import React from "react";
 import { useParams, useHistory } from "react-router-dom";
 import { Container } from "react-bootstrap";
+import useNoteForm from "../../hooks/useNoteForm";
 
 function NoteEdit() {
   const { id } = useParams();
   const history = useHistory();
+
+  const {
+    body,
+    selectedTask,
+    tasks,
+    errors,
+    setErrors,
+    loading,
+  } = useNoteForm(id);
 
   return (
     <Container className="my-4">
