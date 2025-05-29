9cc1be4572f6036ebea14487e781b82267630b02
Alisha <alishaanderssonn@icloud.com>
2025-05-29 22:32:06 +0200

    Add NoteList structure

---
+++ src/pages/notes/NoteList.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+function NoteList() {
+  return (
+    <Container className="my-4">
+      <h2>Notes</h2>
+    </Container>
+  );
+}
+
+export default NoteList;
