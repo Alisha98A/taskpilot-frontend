de7400a033cdc2216250024f7d0d2b9328aa15c4
Alisha <alishaanderssonn@icloud.com>
2025-05-29 23:17:24 +0200

    Add routing for edit params

--- src/pages/notes/NoteEdit.js
+++ src/pages/notes/NoteEdit.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { useParams, useHistory } from "react-router-dom";
+import { Container } from "react-bootstrap";
 
 function NoteEdit() {
   const { id } = useParams();
   const history = useHistory();
 
   return (
-    <div>
-      <h2>Edit Note #{id}</h2>
-    </div>
+    <Container className="my-4">
+      <h2>Edit Note</h2>
+    </Container>
   );
 }
 
