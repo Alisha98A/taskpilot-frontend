1e8b2b9454719050ad851fba068fb8204bbc38af
Alisha <alishaanderssonn@icloud.com>
2025-05-29 23:04:31 +0200

    Add useHistory for navigation

--- src/pages/notes/NoteCreate.js
+++ src/pages/notes/NoteCreate.js
@@ -1,7 +1,11 @@
 import React from "react";
 import { Container } from "react-bootstrap";
+import { useHistory } from "react-router-dom";
+
 
 function NoteCreate() {
+  const history = useHistory();
+
   return (
     <Container className="my-4">
       <h2>Add Note</h2>
