5c48fd3f4a471bdb934877fd075d3773b4288e54
Alisha <alishaanderssonn@icloud.com>
2025-05-29 23:05:41 +0200

    Add useNoteForm hook

--- src/pages/notes/NoteCreate.js
+++ src/pages/notes/NoteCreate.js
@@ -1,10 +1,20 @@
 import React from "react";
 import { Container } from "react-bootstrap";
 import { useHistory } from "react-router-dom";
-
+import useNoteForm from "../../hooks/useNoteForm";
 
 function NoteCreate() {
   const history = useHistory();
+
+  // Use the custom hook to get all form states and setters
+  const {
+    tasks,
+    errors,
+    setErrors,
+    successMessage,
+    setSuccessMessage,
+    loading,
+  } = useNoteForm();
 
   return (
     <Container className="my-4">
