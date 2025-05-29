ef4c2a85a86a3d3f65f1520a1957e8c9a4c4c92b
Alisha <alishaanderssonn@icloud.com>
2025-05-30 01:10:50 +0200

    Initialize file with React imports and empty component

---
+++ src/pages/tasks/TaskForm.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Form, Button, Dropdown } from "react-bootstrap";
+
+function TaskForm() {
+  return <div>Task Form Component</div>;
+}
+
+export default TaskForm;
