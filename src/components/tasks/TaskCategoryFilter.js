9efa5d22dba1435e91da5ac509c7768e533e7441
Alisha <alishaanderssonn@icloud.com>
2025-05-30 13:18:14 +0200

    Setup basic component structure with imports and function skeleton

---
+++ src/components/tasks/TaskCategoryFilter.js
@@ -0,0 +1,11 @@
+import { Button, ButtonGroup } from "react-bootstrap";
+
+function TaskCategoryFilter({ categories, selectedCategory, setSelectedCategory }) {
+  return (
+    <ButtonGroup>
+      {/* Buttons will go here */}
+    </ButtonGroup>
+  );
+}
+
+export default TaskCategoryFilter;
