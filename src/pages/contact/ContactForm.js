0494af1be5cf30b49c2d4178c2d6f93412aff6bf
Alisha <alishaanderssonn@icloud.com>
2025-05-30 14:31:58 +0200

    Add imports and component skeleton

---
+++ src/pages/contact/ContactForm.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import { Form, Button, Container, Row, Col } from "react-bootstrap";
+import { axiosReq } from "../../api/axiosDefaults";
+import styles from "../../styles/ContactForm.module.css";
+
+function ContactForm() {
+  return (
+    <Container className={styles.contactContainer}>
+      <Row className="justify-content-center">
+        <Col md={8} lg={6}>
+          {/* Content will go here */}
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default ContactForm;
