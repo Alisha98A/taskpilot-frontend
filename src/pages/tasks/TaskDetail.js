a96468a71663fc2703947a55bd9ec29945507359
Alisha <alishaanderssonn@icloud.com>
2025-05-30 09:10:45 +0200

    Add basic TaskDetail component with heading

---
+++ src/pages/tasks/TaskDetail.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+function TaskDetail() {
+  return (
+    <Container className="my-4">
+      <h2>Task Detail</h2>
+    </Container>
+  );
+}
+
+export default TaskDetail;
