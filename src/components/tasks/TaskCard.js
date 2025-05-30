ad7ee507ebd1fd0f2e0c68253b7478ed5b419c2d
Alisha <alishaanderssonn@icloud.com>
2025-05-30 13:20:39 +0200

    Add task state labels constant and state selector dropdown

--- src/components/tasks/TaskCard.js
+++ src/components/tasks/TaskCard.js
@@ -3,11 +3,33 @@ import { Card, Form, Badge, Button, Col } from "react-bootstrap";
 import styles from "../../styles/TaskList.module.css";
 
 function TaskCard({ task, updateState, deleteTask }) {
+  // Mapping of task states to user-friendly labels
+  const STATE_LABELS = {
+    open: "Open",
+    in_progress: "In Progress",
+    done: "Done",
+  };
+
   return (
     <Col md={6} lg={4} className="mb-4">
       <Card>
         <Card.Body>
-          {/* Content will go here */}
+          {/* Task state selector */}
+          <div className={styles.taskState}>
+            <strong>State:</strong>
+            <Form.Control
+              as="select"
+              value={task.state}
+              onChange={(e) => updateState(task, e.target.value)}
+              className="mb-2"
+            >
+              {Object.entries(STATE_LABELS).map(([value, label]) => (
+                <option key={value} value={value}>
+                  {label}
+                </option>
+              ))}
+            </Form.Control>
+          </div>
         </Card.Body>
       </Card>
     </Col>
