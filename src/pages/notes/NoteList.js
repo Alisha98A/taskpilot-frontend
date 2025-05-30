fb438c523e675d05063c9b4d50cd0a5f06b3a06e
Alisha <alishaanderssonn@icloud.com>
2025-05-30 13:35:48 +0200

    Import NoteCard component

--- src/pages/notes/NoteList.js
+++ src/pages/notes/NoteList.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Link } from "react-router-dom";
 import {
   Container,
-  Card,
   Button,
   Alert,
   Row,
@@ -11,6 +10,7 @@ import {
 } from "react-bootstrap";
 import useNotesList from "../../hooks/useNotesList";
 import styles from "../../styles/NoteList.module.css";
+import NoteCard from "../../components/NoteCard";
 
 function NoteList() {
   const { notes, loading, error } = useNotesList();
@@ -51,32 +51,7 @@ function NoteList() {
         <Row xs={1} sm={2} md={3} lg={4} className="g-4">
           {notes.map((note) => (
             <Col key={note.id}>
-              <Card className={styles.noteCard} role="article" tabIndex="0">
-                <Card.Body className="d-flex flex-column justify-content-between">
-                  <div>
-                    <Card.Title className={styles.noteTitle}>
-                      <i className="fas fa-sticky-note me-2 text-primary"></i>
-                      <Link to={`/notes/${note.id}`} className={styles.noteLink}>
-                        {note.body.length > 80
-                          ? note.body.substring(0, 80) + "..."
-                          : note.body}
-                      </Link>
-                    </Card.Title>
-
-                    <Card.Text className="text-muted d-flex align-items-center">
-                      <i className="fas fa-tasks me-2"></i>
-                      <Link to={`/tasks/${note.task.id}`} className={styles.taskLink}>
-                        {note.task.title}
-                      </Link>
-                    </Card.Text>
-                  </div>
-
-                  <small className={styles.timestamp}>
-                    <i className="fas fa-clock me-1"></i>
-                    {new Date(note.date_added).toLocaleString()}
-                  </small>
-                </Card.Body>
-              </Card>
+              <NoteCard note={note} />
             </Col>
           ))}
         </Row>
