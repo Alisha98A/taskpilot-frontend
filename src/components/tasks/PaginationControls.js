890e7454285b2e2990e5935ff33d574a7e86a067
Alisha <alishaanderssonn@icloud.com>
2025-05-30 13:25:07 +0200

    Basic component structure with imports and function skeleton

---
+++ src/components/tasks/PaginationControls.js
@@ -0,0 +1,11 @@
+import { Button, ButtonGroup } from "react-bootstrap";
+
+function PaginationControls({ total, currentPage, setCurrentPage, tasksPerPage }) {
+  return (
+    <div>
+      {/* Pagination buttons will go here */}
+    </div>
+  );
+}
+
+export default PaginationControls;
