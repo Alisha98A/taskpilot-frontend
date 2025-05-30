13c443589cd9de1524b38b184f19c3de42b3105b
Alisha <alishaanderssonn@icloud.com>
2025-05-30 09:11:04 +0200

    Use useParams to extract task ID from URL

--- src/pages/tasks/TaskDetail.js
+++ src/pages/tasks/TaskDetail.js
@@ -1,7 +1,10 @@
 import React from "react";
 import { Container } from "react-bootstrap";
+import { useParams } from "react-router-dom";
 
 function TaskDetail() {
+  const { id } = useParams(); // Extract task ID from URL parameters
+
   return (
     <Container className="my-4">
       <h2>Task Detail</h2>
