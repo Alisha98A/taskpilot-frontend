9cf811e41bfa62cfc146fdd54c32e0293a6025bd
Alisha <alishaanderssonn@icloud.com>
2025-05-30 13:25:35 +0200

    Add calculation for total pages and early return if only one or no pages

--- src/components/tasks/PaginationControls.js
+++ src/components/tasks/PaginationControls.js
@@ -1,6 +1,12 @@
 import { Button, ButtonGroup } from "react-bootstrap";
 
 function PaginationControls({ total, currentPage, setCurrentPage, tasksPerPage }) {
+  // Calculate total number of pages based on total items and items per page
+  const totalPages = Math.ceil(total / tasksPerPage);
+
+  // Don't render pagination if only one or no pages
+  if (totalPages <= 1) return null;
+
   return (
     <div>
       {/* Pagination buttons will go here */}
